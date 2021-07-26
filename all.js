6d54f9c14c34b6156ec7895cce09f8a80b82bd3c
Vivian Yeh <u4h93u6uhu@gmail.com>
2021-07-26 14:21:13 +0800

    modify container's size & add sunglasses page(RWD)

--- all.js
+++ all.js
@@ -3,15 +3,23 @@ const app = {
         return {
         index:true,
         optical:false,
+        sunglasses: false
     }},
     methods: {
         showIndex(){
             this.index = true;
-            this.optical = false
+            this.optical = false;
+            this.sunglasses = false;
         },
         showOptical(){
             this.optical = true
             this.index = false;
+            this.sunglasses = false;
+        },
+        showSunglasses(){
+            this.sunglasses = true;
+            this.index = false;
+            this.optical = false
         }
     }
 }
